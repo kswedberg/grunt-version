502c64b7d9af0a7cf9e4a124793a5046767860d8
Karl Swedberg <kswedberg@gmail.com>
2013-02-16 22:25:27 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+/*
+ * grunt-version
+ * https://github.com/kswedberg/grunt-version
+ *
+ * Copyright (c) 2013 Karl Swedberg
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+    copy: {
+      tests: {
+        files: [{
+          src: ['test/fixtures/*'],
+          dest: 'tmp/',
+          filter: 'isFile',
+          expand: true,
+          flatten: true
+        }]
+      }
+    },
+    // Configuration to be run (and then tested).
+    version: {
+      options: {
+        pkg: grunt.file.readJSON('test/fixtures/test-package.json')
+      },
+      prefix_option: {
+        prefix: 'version[\'"]?( *=|:) *[\'"]',
+        src: ['tmp/testing.js', 'tmp/testingb.js'],
+      },
+      release_option: {
+        options: {
+          release: 'patch'
+        },
+        src: [
+          'tmp/123.js',
+          'tmp/456.js',
+          'tmp/test-package.json'
+        ]
+      },
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'copy', 'version', 'nodeunit' ]);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/version.js
@@ -0,0 +1,64 @@
+/*
+ * grunt-version
+ * https://github.com/kswedberg/grunt-version
+ *
+ * Copyright (c) 2013 Karl Swedberg
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('version', 'Update version number in all the files.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      prefix: 'version[\'"]?\\s*[:=]\\s*[\'"]',
+      pkg: grunt.config('pkg'),
+      release: false
+    });
+
+    var newVersion,
+        semver = require('semver'),
+        version = options.pkg.version;
+
+    if (options.release) {
+      newVersion = semver.inc(version, options.release);
+    }
+    version = newVersion || version;
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(fileObj) {
+      // The source files to be modified. The "nonull" option is used
+      // to retain invalid files/patterns so they can be warned about.
+      var files = grunt.file.expand({nonull: true}, fileObj.src);
+
+      files.forEach(function(filepath) {
+        // Warn if a source file/pattern was invalid.
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.error('Source file "' + filepath + '" not found.');
+          return '';
+        }
+        // Read file source.
+        var pattern = new RegExp('(' + options.prefix + ')[0-9a-zA-Z\\-_\\.]+', 'g'),
+            file = grunt.file.read(filepath),
+            newfile = file.replace(pattern, '$1' + version);
+
+        if (!pattern.test(file)) {
+          grunt.log.subhead('Pattern not found');
+          grunt.log.writeln('Path: ' + filepath);
+          grunt.log.writeln('Pattern: ' + pattern);
+        } else {
+
+          grunt.log.subhead('File updated');
+          grunt.log.writeln('Path: ' + filepath);
+          grunt.log.writeln('Version: ' + version);
+
+        }
+        grunt.file.write(filepath, newfile);
+      });
+
+    });
+  });
+
+};
